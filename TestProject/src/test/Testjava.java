1f1bb6c9d9e20a2c15f747192318dba833313ab2
NeoJinE <lje845223@gmail.com>
2024-06-19 02:43:48 +0900

    Java

---
+++ TestProject/src/test/Testjava.java
@@ -0,0 +1,40 @@
+package test;
+
+public class A {
+	//자동실행되지 않는다=> class B가 public이 아니고, 파일명과 동일한 이름의 클래스가 아니기 떄문.
+	public static void main(String[] args) {//main 함수 내의 코드는 jvm이 프로그램을 시작시키는 시작점.
+		// 테스트입니다 출력하는 코드를 작성.
+		//System.out.println:java 언어의 표준 출력장치로 출력 명령어
+		//화면에 () 내용이 출력된다 => eclipse에서는 console탭에 결과출력
+		System.out.println("test입니다.");
+		
+	}
+
+}
+
+class B{
+	//자동실행되지 않는다=> class B가 public이 아니고, 파일명과 동일한 이름의 클래스가 아니기 떄문.
+	public static void main(String[] args) {//main 함수 내의 코드는 jvm이 프로그램을 시작시키는 시작점.
+		// 테스트입니다 출력하는 코드를 작성.
+		//System.out.println:java 언어의 표준 출력장치로 출력 명령어
+		//화면에 () 내용이 출력된다 => eclipse에서는 console탭에 결과출력
+		System.
+		out.
+		println("test"
+				+"입니다."); //모두 한줄 문자으로 표현된다.
+		
+	}
+	
+}
+
+public class Testjava {
+	//자동 실행되는 메인 함수=> public class이고, 파일명과 클래스명이 동일하기 때문이다.
+	public static void main(String[] args) {//main 함수 내의 코드는 jvm이 프로그램을 시작시키는 시작점.
+		// 테스트입니다 출력하는 코드를 작성.
+		//System.out.println:java 언어의 표준 출력장치로 출력 명령어
+		//화면에 () 내용이 출력된다 => eclipse에서는 console탭에 결과출력
+		System.out.println("test입니다.");
+		
+	}
+
+}
